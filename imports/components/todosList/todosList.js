a697037882af7698b4a2f708d252e1ddfdf23f87
Justin McMinn <akajut@gmail.com>
2016-05-05 00:06:19 -0500

    7.3 Add incompleteCount helper to component

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -8,14 +8,32 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
+        const selector = {};
+
+        // if hide completed is check, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
         // Show newest tasks at the top
-        return Tasks.find({}, {
+        return Tasks.find(selector, {
           sort: {
             createdAt: -1
           }
         });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
       }
     })
   }
