d24d1eac5422f3e3b41b76d1720de186b1db9cb4
Justin McMinn <akajut@gmail.com>
2016-05-05 16:56:16 -0500

    11.7 Add test to check incomplete tasks count

--- imports/components/todosList/client/todosList.tests.js
+++ imports/components/todosList/client/todosList.tests.js
@@ -23,5 +23,10 @@ describe('todosList', function() {
     element = $compile('<todos-list></todos-list>')($rootScope.$new(true));
     $rootScope.$digest();
   });
+
+  describe('component', function() {
+    it('should be showing incomplete tasks count', function() {
+      assert.include(element[0].querySelector('h1').innerHTML, '0');
+    });
   });
 })
